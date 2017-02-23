ac9ff35e902e8797eddf97410e8786545d39aae4
JacobyJoukema <joukemjw@mcmaster.ca>
2017-02-23 11:46:10 -0500

    Removed Test and added RNN and midi_manip

---
+++ src/RNN.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+import midi_manipulation
+
+lowest = midi_manipulation.lowerBound
+highest = midi_manipulation.uppderBound
+nRange = highest-lowest
+
+numTimesteps = 20
+visible =2*nRange*numTimesteps
+hidden = 50
